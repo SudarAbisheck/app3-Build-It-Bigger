d4f3cd2b3811722c9a57675019b6d5559e07b442
Sudar Abisheck <sudarabisheck@gmail.com>
2016-06-23 00:00:24 +0530

    manifest changes made

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -3,10 +3,12 @@ package com.udacity.gradle.builditbigger;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
